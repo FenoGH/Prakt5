cb0f2c9e40db5dfe7df7963d455f8fc3984fd803
qwezs <qwezs@DESKTOP-5HC4037>
2022-10-29 04:35:57 +0300

    Добавьте файлы проекта.

---
+++ Практ5/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Практ5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Pair firstpara = new Pair();
+        Pair secondpara = new Pair();
+        Pair thirdpara = new Pair();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void AboutClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Борькин Иван ИСП-31 \nСоздать класс Pair (пара четных чисел). Создать необходимые методы и свойства. Определить метод вычисления произведения чисел.Создать перегруженный метод для вычисления произведения 2 пар чисел – (а, b) * (с, d) = (a * c), (b * d)");
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+       
+
+        private void OneParaClick(object sender, RoutedEventArgs e)
+        {
+            Rez.Clear();
+            int.TryParse(rd1.Text, out int ch1); int.TryParse(rd2.Text, out int ch2);
+            if (ch1 % 2 == 0 && ch2 % 2 == 0)
+            {
+                firstpara.Para1 = ch1; firstpara.Para2 = ch2;
+                Rez.Text = Convert.ToString($"Созданная пара ({firstpara.Para1};{firstpara.Para2})");
+            }
+            else
+            {
+                firstpara.Para1 = ch1; firstpara.Para2 = ch2;
+            }
+        }
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(rd1.Text, out int ch1); int.TryParse(rd2.Text, out int ch2); int.TryParse(rd3.Text, out int ch3); int.TryParse(rd4.Text, out int ch4);
+            if (ch1 % 2 == 0 && ch2 % 2 == 0 && ch3 % 2 == 0 && ch4 % 2 == 0)
+            {
+                firstpara.Para1 = ch1; firstpara.Para2 = ch2;
+                secondpara.Para1 = ch3; secondpara.Para2 = ch4;
+                thirdpara.multiplication(firstpara, secondpara, out int par1, out int par2);
+                Rez.Text = Convert.ToString($"Созданная пара 1 ({firstpara.Para1};{firstpara.Para2}) \nСозданная пара 2 ({secondpara.Para1};{secondpara.Para2})\nРезультат умножения пар ({par1};{par2})");
+            }
+            else
+            {
+                firstpara.Para1 = ch1; firstpara.Para2 = ch2;
+                secondpara.Para1 = ch3; secondpara.Para2 = ch4;
+            }
+        }
+
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            rd1.Clear();
+            rd2.Clear();
+            rd3.Clear();
+            rd4.Clear();
+            Rez.Clear();
+
+        }
+    }
+}
---
+++ Практ5/Pair.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Практ5
+{
+     class Pair
+    {
+        //public int Para1 { get; set; }
+        //public int Para2 { get; set; }
+        private int para1;
+        private int para2;
+        public int Para1
+        {
+            get
+            {
+                return para1;
+            }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    para1 = value;
+                }
+                else
+                {
+                    MessageBox.Show("Первое число в паре не четное");
+                }
+
+            }
+        }
+        public int Para2
+        {
+            get
+            {
+                return para2;
+            }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    para2 = value;
+                }
+                else
+                {
+                    MessageBox.Show("Второе число в паре не четное");
+                }
+            }
+        }
+        public void multiplication(Pair value1, Pair value2, out int par1,out int par2)
+        {
+            par1 = value1.Para1 * value2.Para1;
+            par2 = value1.Para2 * value2.Para2;
+        }
+    }
+}
