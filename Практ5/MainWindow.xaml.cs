8dbd1b2065b89eeb3b08c66f9a1bdd675b8816dc
qwezs <qwezs@DESKTOP-5HC4037>
2022-10-29 15:59:27 +0300

    Fix

--- Практ5/MainWindow.xaml.cs
+++ Практ5/MainWindow.xaml.cs
@@ -21,9 +21,7 @@ namespace Практ5
     /// </summary>
     public partial class MainWindow : Window
     {
-        Pair firstpara = new Pair();
-        Pair secondpara = new Pair();
-        Pair thirdpara = new Pair();
+        Pair firstpara = new Pair();Pair secondpara = new Pair();Pair thirdpara = new Pair();
         public MainWindow()
         {
             InitializeComponent();
@@ -62,26 +60,19 @@ namespace Практ5
             int.TryParse(rd1.Text, out int ch1); int.TryParse(rd2.Text, out int ch2); int.TryParse(rd3.Text, out int ch3); int.TryParse(rd4.Text, out int ch4);
             if (ch1 % 2 == 0 && ch2 % 2 == 0 && ch3 % 2 == 0 && ch4 % 2 == 0)
             {
-                firstpara.Para1 = ch1; firstpara.Para2 = ch2;
-                secondpara.Para1 = ch3; secondpara.Para2 = ch4;
+                firstpara.Para1 = ch1; firstpara.Para2 = ch2;secondpara.Para1 = ch3; secondpara.Para2 = ch4;
                 thirdpara.multiplication(firstpara, secondpara, out int par1, out int par2);
                 Rez.Text = Convert.ToString($"Созданная пара 1 ({firstpara.Para1};{firstpara.Para2}) \nСозданная пара 2 ({secondpara.Para1};{secondpara.Para2})\nРезультат умножения пар ({par1};{par2})");
             }
             else
             {
-                firstpara.Para1 = ch1; firstpara.Para2 = ch2;
-                secondpara.Para1 = ch3; secondpara.Para2 = ch4;
+                firstpara.Para1 = ch1; firstpara.Para2 = ch2;secondpara.Para1 = ch3; secondpara.Para2 = ch4;
             }
         }
 
         private void ClearClick(object sender, RoutedEventArgs e)
         {
-            rd1.Clear();
-            rd2.Clear();
-            rd3.Clear();
-            rd4.Clear();
-            Rez.Clear();
-
+            rd1.Clear();rd2.Clear();rd3.Clear();rd4.Clear();Rez.Clear();
         }
     }
 }
